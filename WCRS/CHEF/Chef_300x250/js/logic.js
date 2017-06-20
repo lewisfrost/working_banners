ce85222285d39071ce448452a1f47d5700908a2c
Lewis Frost <lewis@lewisfrost.com>
2017-06-20 13:15:17 +0100

    add navy

---
+++ WCRS/CHEF/Chef_300x250/js/logic.js
@@ -0,0 +1,63 @@
+function addevents(){
+	document.getElementById("bannerCTA").addEventListener("click", handleClickthroughButtonClick);
+	document.getElementById("cta").addEventListener("click", handleClickthroughButtonClick);
+}
+
+function handleClickthroughButtonClick(){
+	window.open(clickTag,'_blank'); 
+}
+
+function frame1Animation(){
+	TweenLite.set([img1,logo1],{opacity:1});
+	TweenLite.to(copy1,0.65,{opacity:1, ease:Quad.easeIn, delay:0});
+	TweenLite.delayedCall(3,frame2Animation);
+}
+function frame2Animation(){
+	TweenLite.to(copy1,0.65,{opacity:0, ease:Quad.easeOut, delay:0});
+	TweenLite.to(copy2,0.65,{opacity:1, ease:Quad.easeIn, delay:0.65});
+	TweenLite.delayedCall(3.65,frame3Animation);
+}
+function frame3Animation(){
+	TweenLite.to([copy2,logo1],0.65,{opacity:0, ease:Quad.easeOut, delay:0});
+	TweenLite.to(gradient,0.65,{opacity:1, ease:Quad.easeIn, delay:0});
+	TweenLite.to(logo2,0.65,{opacity:1, ease:Quad.easeIn, delay:0.65});
+	TweenLite.to(end_copy1,0.65,{opacity:1, ease:Quad.easeIn, delay:1.3});
+	TweenLite.to(cta,0.65,{opacity:1, ease:Quad.easeIn, delay:1.65});
+}
+
+	
+// images to preload
+var images = [
+	'images/img1.jpg',
+	'images/copy1.png',
+	'images/copy2.png',
+	'images/cta.png',
+	'images/end_copy1.png',
+	'images/gradient.png',
+	'images/logo1.png',
+	'images/logo2.png'
+];
+
+function preloadImages(urls, callback) {
+	var length = urls.length, loaded = 0;
+	for (var count = 0; count < length; count++) {
+
+		var img = new Image();
+		img.onload = function() {
+			if (++loaded == length && callback) callback();
+		}
+		img.src = urls[count];
+	}
+}
+
+function todofunc(){
+	console.log('***run todo');
+	frame1Animation();
+	addevents()
+}
+
+function initBanner(){
+	console.log('***init');
+	preloadImages(images, todofunc);
+    
+}
